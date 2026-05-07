c7b80c29ff0753c889417b01fd089c1285abf2dc
Thiago Azevedo <thiago.parreirass@gmail.com>
2026-05-07 19:39:37 -0300

    Add Files

---
+++ RachaAi4261210565/RachaAi4261210565/Models/Grupo.cs
@@ -0,0 +1,9 @@
+﻿namespace RachaAi4261210565.Models;
+
+public class Grupo
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Categoria { get; set; } = string.Empty;
+    public decimal ValorPendente { get; set; }
+    public bool NoVermelho { get; set; }
+}
